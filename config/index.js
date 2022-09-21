242bec5ad5de7f03791d9beb43b664ef47d45d5f
Driannnnnn <114047079+Driannnnnn@users.noreply.github.com>
2022-09-21 15:20:38 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -57,7 +57,7 @@ export const config = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USER: ["oCZRU6uVuDIxetNirz0zubEHz5kI",
+  USERS: ["oCZRU6uVuDIxetNirz0zubEHz5kI",
   {
       // 想要发送的人的名字
       name: "千千小朋友",
